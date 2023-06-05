9a152e3ff517d46ed2967973c769e811d8192682
EliteMasterEric <ericmyllyoja@gmail.com>
2023-06-05 16:03:57 -0400

    Additional documentation.

--- src/main/java/io/github/thebusybiscuit/mobcapturer/adapters/mobs/StandardMobAdapter.java
+++ src/main/java/io/github/thebusybiscuit/mobcapturer/adapters/mobs/StandardMobAdapter.java
@@ -6,6 +6,12 @@ import org.bukkit.entity.Mob;
 
 import io.github.thebusybiscuit.mobcapturer.adapters.MobAdapter;
 
+/**
+ * This is an adapter used for any mobs which have no extra attributes to store,
+ * other than those stored for all living entities.
+ * 
+ * Mobs like Spiders, Squids, Bats, Ghasts, Salmon, Cod, etc...
+ */
 public class StandardMobAdapter<T extends Mob> implements MobAdapter<T> {
 
     private final Class<T> entityClass;
