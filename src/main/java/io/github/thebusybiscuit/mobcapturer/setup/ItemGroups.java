6fc26c35e9bd6a66303ec251acf6cea54c016a53
ybw0014 <i@guizhanss.com>
2023-09-21 18:11:22 -0400

    feat(CustomModelData): Add id for category items

--- src/main/java/io/github/thebusybiscuit/mobcapturer/setup/ItemGroups.java
+++ src/main/java/io/github/thebusybiscuit/mobcapturer/setup/ItemGroups.java
@@ -1,5 +1,7 @@
 package io.github.thebusybiscuit.mobcapturer.setup;
 
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 
@@ -19,8 +21,9 @@ import io.github.thebusybiscuit.slimefun4.utils.SlimefunUtils;
 public final class ItemGroups {
     public static final NestedItemGroup MAIN = new NestedItemGroup(
         new NamespacedKey(MobCapturer.getInstance(), "mob_capturer"),
-        new CustomItemStack(
-            SlimefunUtils.getCustomHead("d429ff1d2015cb11398471bb2f895f7b4c3ccec201e4ad7a86ff24b744878c"),
+        new SlimefunItemStack(
+            "MOB_CAPTURER",
+            "d429ff1d2015cb11398471bb2f895f7b4c3ccec201e4ad7a86ff24b744878c",
             "&dMob Capturer"
         )
     );
@@ -28,7 +31,7 @@ public final class ItemGroups {
         new NamespacedKey(MobCapturer.getInstance(), "tools"),
         MAIN,
         new CustomItemStack(
-            Material.BLAZE_ROD,
+            ItemStacks.MOB_CANNON,
             "&dTools"
         )
     );
